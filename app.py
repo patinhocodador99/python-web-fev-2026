77ee00b1a71fb1441a36c05528b7844e649d8781
Patinho Codador <sthefanie.silvaifc00@gmail.com>
2026-02-12 09:59:20 -0300

    Atualização de arquivos do projeto

--- app.py
+++ app.py
@@ -1,8 +1,27 @@
-from flask import Flask
+import os
+from flask import Flask, render_template
+from dotenv import load_dotenv
+
+load_dotenv() 
 
 app = Flask(__name__)
 
 @app.route('/')
-
 def home():
-    return "Hello, World!"
+    return render_template('index.html')
+
+@app.route('/nome')
+
+def exibir_nome():
+    return "Danilo"
+
+@app.route('/nome/sobrenome')
+def exibir_sobrenome():
+    return "Souza Miguel"
+
+if __name__ == '__main__':
+    app.run(
+        host=os.getenv("FLASK_HOST"),
+        port=os.getenv("FLASK_PORT"),
+        debug=os.getenv("FLASK_DEBUG") == "1"
+        )
